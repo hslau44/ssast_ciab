d4153f2207e579a1a5bad1434e2e0ecdc406dade
hslau44 <hslau44@gmail.com>
2023-07-16 18:09:04 +0000

    add script to install recorder

---
+++ install_recorder.sh
@@ -0,0 +1,4 @@
+curl -JLO https://github.com/stefanrmmr/streamlit_audio_recorder/archive/refs/heads/main.zip
+unzip streamlit_audio_recorder-main.zip
+mv streamlit_audio_recorder-main recorder
+rm streamlit_audio_recorder-main.zip
